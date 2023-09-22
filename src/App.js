60c066d787562d7f135edde991a38373acbd668f
Katia <kushnir.ekaterina@gmail.com>
2023-09-22 13:02:29 +0200

    Upload the project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { app } from './firebase';
+
+function App() {
+  return (
+    <div>
+      Welcome to FireShopping
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC-XkQD8G1X2dC8mJdO1aQsk1Qt1wyJeXc",
+    authDomain: "fir-shopping-cf46e.firebaseapp.com",
+    projectId: "fir-shopping-cf46e",
+    storageBucket: "fir-shopping-cf46e.appspot.com",
+    messagingSenderId: "895331878610",
+    appId: "1:895331878610:web:989111fdc8f5359d25cf0b"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
